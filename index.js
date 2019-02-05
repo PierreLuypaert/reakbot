30d02f4e9391c65e8f28a4af72765bd8f0ff78af
Sneawy <47297601+Sneawy@users.noreply.github.com>
2019-02-05 20:05:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bot = new Discord.Client();
 var couleur = 1
 bot.on('ready', () => {
     console.log("Je suis prêt!");
+    bot.user.setGame('Par Sneawy')
     bot.channels.get("528998224380559402").send({embed: {
         color: 0x800080,
         author: {
